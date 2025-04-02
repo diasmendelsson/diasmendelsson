f5839548927da44a51e10680a96e88beeac1bed7
diasmendelsson <mendersson@hotmail.com>
2025-04-02 20:12:35 -0300

    Deletado alguns arquivos e editado componente Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,24 @@
+'use client'
+import Image from "next/image"
+import { Fade as Hamburger } from 'hamburger-react'
 
 export default function Header() {
     return (
-        <div>Header</div>
+
+    <header className="h-14 items-center px-4 py-12 flex justify-between">
+        <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+                <div className="w-4 h-4 bg-blue-700"></div>
+                <h1 className="text-2xl  font-bold">Mendelsson Dias</h1>
+            </div>
+            
+
+            <Image className="hidden" src="/barra.svg" alt="icone de barra" width={20} height={10} />
+
+            <p className="text-xl font-extralight uppercase tracking-wide">Desenvolvedor Web</p>
+        </div>
+
+        <Hamburger rounded color="#0050ff"/>
+    </header>
     )
 }
